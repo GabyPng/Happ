c61984b411598b4ddf33d11f7db0ac21d5085925
gabypng <gaby.paniaguag@gmail.com>
2025-07-22 23:53:49 -0700

    Conexion con la base de datos y clave oculta, diferente usuario

--- src/db.js
+++ src/db.js
@@ -1,16 +1,27 @@
+require('dotenv').config();
 
-const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://gabypaniaguag:<db_password>@happiety.olrm8hw.mongodb.net/?retryWrites=true&w=majority&appName=HappiEty"; 
-const client = new MongoClient(uri);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URI;
 
-async function connectToDB() {
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    console.log("MongoDB conectado");
-    return client.db("happiety"); git 
-  } catch (err) {
-    console.error("Error al conectar a MongoDB:", err);
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
-
-module.exports = connectToDB;
+run().catch(console.dir);
